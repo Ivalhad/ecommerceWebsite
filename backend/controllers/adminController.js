c13edeab8255e34618319c634e0bb176161e6ac7
ivalhad <ivan14hadrian@gmail.com>
2025-11-24 00:30:25 +0700

    refactor admin controller with global middleware

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,58 +1,58 @@
+// File: backend/controllers/adminController.js
+const asyncHandler = require('express-async-handler'); // Import library tadi
 const User = require('../models/userModel');
 const generateToken = require('../utils/generateToken');
 const Blacklist = require('../models/tokenBlacklist');
 
-//login admin
-const authAdmin = async (req, res) => {
-    const {email, password} = req.body;
+// admin auth
+const authAdmin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    const user = await User.findOne ({ email });
-    //cek admin ada atau engga
-    if (user && (await user.matchPassword(password)) && user.role === 'admin') {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            role: user.role,
-            token: generateToken(user._id),
-        })
-    } else {
-        res.status(401).json({message: 'Invalid email or password'});
-    }
-}
+  const user = await User.findOne({ email });
 
-//admin profile
-const getAdminProfile = async (req, res) => {
+  if (user && (await user.matchPassword(password)) && user.role === 'admin') {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
 
-    const user = await User.findById(req.user._id);
-
-    if (user) {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            role: user.role,
-            phoneNumber: user.phoneNumber,
-        });
-    } else {
-        res.status(404).json({ message: 'Admin not found' });
-    }
-};
-
-//admmin update
-const updateAdminProfile = async (req, res) => {
-  
+// get admin profile
+const getAdminProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
   if (user) {
-    // update data jika ada input baru
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      phoneNumber: user.phoneNumber,
+    });
+  } else {
+    res.status(404);
+    throw new Error('Admin not found');
+  }
+});
+
+// update admin profile
+const updateAdminProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
     user.phoneNumber = req.body.phoneNumber || user.phoneNumber;
 
-    // cek admmin mengirim password baru
     if (req.body.password) {
-      user.password = req.body.password; 
+      user.password = req.body.password;
     }
 
     const updatedUser = await user.save();
@@ -63,36 +63,30 @@ const updateAdminProfile = async (req, res) => {
       email: updatedUser.email,
       role: updatedUser.role,
       phoneNumber: updatedUser.phoneNumber,
-      //kirim token baru
-      token: generateToken(updatedUser._id), 
+      token: generateToken(updatedUser._id),
     });
   } else {
-    res.status(404).json({ message: 'User not found' });
+    res.status(404);
+    throw new Error('Admin not found');
   }
-};
+});
 
-//admin logout
-const logoutAdmin = async (req, res) => {
-  try {
-    // token dari header
-    const token = req.headers.authorization.split(' ')[1];
+// admin logout
+const logoutAdmin = asyncHandler(async (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
 
-    if (!token) {
-        return res.status(400).json({ message: 'Token not found' });
-    }
-
-    // masukkan token ke Blacklist
-    const blacklistedToken = new Blacklist({
-      token: token
-    });
-
-    await blacklistedToken.save();
-  
-    res.status(200).json({ message: 'Admin logged out successfully & token invalidated' });
-  } catch (error) {
-    res.status(500).json({ message: 'Logout failed', error: error.message });
+  if (!token) {
+    res.status(400);
+    throw new Error('Token not found');
   }
-};
 
+  const blacklistedToken = new Blacklist({
+    token: token,
+  });
 
-module.exports = { authAdmin, getAdminProfile, updateAdminProfile, logoutAdmin};
+  await blacklistedToken.save();
+
+  res.status(200).json({ message: 'Admin logged out successfully' });
+});
+
+module.exports = { authAdmin, getAdminProfile, updateAdminProfile, logoutAdmin,};
