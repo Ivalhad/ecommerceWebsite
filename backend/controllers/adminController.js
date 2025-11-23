f88a2650de8988b339055b65099963e8dea3a4b7
ivalhad <ivan14hadrian@gmail.com>
2025-11-23 20:29:06 +0700

    update admin controller

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,23 @@
+const user = require('../models/userModel');
+const generateToken = require('..utils/genereteToken');
+
+//login admin
+const authAdmin = async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await user.findOne ({ email });
+    //cek admin ada atau engga
+    if (user && (await user.matchPassword(password)) && user.role === 'admin') {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(401).json({message: 'Invalid email or password'});
+    }
+}
+
+module.exports = { authAdmin };
