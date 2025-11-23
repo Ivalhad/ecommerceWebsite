a5b97d14d7eded743b52bc235fa1e1b6b1923069
ivalhad <ivan14hadrian@gmail.com>
2025-11-23 23:27:01 +0700

    update routes admin (router admin logout)

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { authAdmin, getAdminProfile, updateAdminProfile } = require('../controllers/adminController');
+const { authAdmin, getAdminProfile, updateAdminProfile, logoutAdmin } = require('../controllers/adminController');
 const { protect, admin } = require ('../middleware/authMiddleware')
 
 //route admin login
 router.post('/login', authAdmin);
+
 //route admin profile
 router.route('/profile')
     .get(protect, admin, getAdminProfile)
-    .put(protect, admin, updateAdminProfile); 
+    .put(protect, admin, updateAdminProfile);
+
+//route admin logout
+router.post('/logout', logoutAdmin);
 
 module.exports = router;
