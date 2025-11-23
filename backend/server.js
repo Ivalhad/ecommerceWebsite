e635096458e233e9133c1070d2103817bf5ec315
ivalhad <ivan14hadrian@gmail.com>
2025-11-23 17:57:12 +0700

    updatee server.js first config

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors()); 
+app.use(express.json());
+
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
