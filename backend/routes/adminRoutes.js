9ea298f490877eee61e555b503b6d14f23b5518a
ivalhad <ivan14hadrian@gmail.com>
2025-11-23 22:06:55 +0700

    update router admin (get profile admin)

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {authAdmin} = require('../controllers/adminController');
+const {authAdmin, getAdminProfile} = require('../controllers/adminController');
+const { protect, admin } = require ('../middleware/authMiddleware')
 
 //route admin login
 router.post('/login', authAdmin);
+//route admin profile
+router.get('/profile', protect, admin, getAdminProfile)
 
 module.exports = router;
