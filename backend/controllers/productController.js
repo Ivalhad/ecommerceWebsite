772bf97a92e8cddc90649984688eec7ffadfddeb
ivalhad <ivan14hadrian@gmail.com>
2025-11-24 21:37:33 +0700

    refactor product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,34 +1,23 @@
 const asyncHandler = require('express-async-handler');
 const Product = require('../models/productModel');
 
-// get all products
-const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-});
-
-// get product by id
-const getProductById = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
-
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404);
-    throw new Error('Product not found');
-  }
-});
-
 // create new product
 const createProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, brand, category, countInStock } = req.body;
 
-  const { name, price, description, image, brand, category, countInStock } = req.body;
+  if (!req.file) {
+    res.status(400);
+    throw new Error('Please upload an image');
+  }
+
+  // get path image
+  const imagePath = `/${req.file.path.replace(/\\/g, '/')}`;
 
   const product = new Product({
+    user: req.user._id, 
     name,
     price,
-    user: req.user._id,
-    image, 
+    image: imagePath,
     brand,
     category,
     countInStock,
@@ -39,23 +28,17 @@ const createProduct = asyncHandler(async (req, res) => {
   res.status(201).json(createdProduct);
 });
 
-// update product
-const updateProduct = asyncHandler(async (req, res) => {
-  const { name, price, description, image, brand, category, countInStock } = req.body;
+// get all products
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
 
+// get product by id
+const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
-
   if (product) {
-    product.name = name || product.name;
-    product.price = price || product.price;
-    product.description = description || product.description;
-    product.image = image || product.image;
-    product.brand = brand || product.brand;
-    product.category = category || product.category;
-    product.countInStock = countInStock || product.countInStock;
-
-    const updatedProduct = await product.save();
-    res.json(updatedProduct);
+    res.json(product);
   } else {
     res.status(404);
     throw new Error('Product not found');
@@ -65,10 +48,34 @@ const updateProduct = asyncHandler(async (req, res) => {
 // delete product
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.deleteOne();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+// update procuct
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, brand, category, countInStock } = req.body;
+  const product = await Product.findById(req.params.id);
 
   if (product) {
-    await product.deleteOne(); 
-    res.json({ message: 'Product removed' });
+    product.name = name || product.name;
+    product.price = price || product.price;
+    product.description = description || product.description;
+    product.brand = brand || product.brand;
+    product.category = category || product.category;
+    product.countInStock = countInStock || product.countInStock;
+    
+    if (req.file) {
+      product.image = `/${req.file.path.replace(/\\/g, '/')}`;
+    }
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
   } else {
     res.status(404);
     throw new Error('Product not found');
@@ -80,5 +87,5 @@ module.exports = {
   getProductById,
   createProduct,
   updateProduct,
-  deleteProduct,
+  deleteProduct
 };
