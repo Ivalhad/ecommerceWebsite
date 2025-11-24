14d19ef9c616184fc3195ae9039021e08bd83864
ivalhad <ivan14hadrian@gmail.com>
2025-11-24 19:58:43 +0700

    update new function product moodel

---
+++ backend/models/productModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  // Kita simpan ID Admin yang menginput barang ini (Relasi ke User)
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User', 
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+}, {
+  timestamps: true,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
