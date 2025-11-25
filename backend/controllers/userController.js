aeb033f3a863e2b30c8c71cf5658987d545c9628
ivalhad <ivan14hadrian@gmail.com>
2025-11-25 17:29:51 +0700

    add login functionality to user controller and routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,7 +34,28 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+//login 
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
 
 module.exports = {
     registerUser,
+    loginUser,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
   registerUser,
+  loginUser,
 } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 // register
 router.post('/register', registerUser);
+router.post('/login', loginUser)
 
 module.exports = router;
