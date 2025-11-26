d1f3dedf2375203dfe96c43cd3b1e2eb01d35306
ivalhad <ivan14hadrian@gmail.com>
2025-11-27 00:16:47 +0700

    refactor addOrderItems: fetch real product data and calculate order prices

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Order = require('../models/orderModel');
+const Product = require('../models/productModel');
 
 // create new order
 const addOrderItems = asyncHandler(async (req, res) => {
@@ -7,17 +8,43 @@ const addOrderItems = asyncHandler(async (req, res) => {
     orderItems,
     shippingAddress,
     paymentMethod,
-    itemsPrice,
-    shippingPrice,
-    totalPrice,
   } = req.body;
 
   if (orderItems && orderItems.length === 0) {
     res.status(400);
     throw new Error('No order items');
   } else {
+    // get real product data from DB
+    const orderItemsWithRealData = await Promise.all(
+      orderItems.map(async (item) => {
+        // search by id
+        const product = await Product.findById(item.product);
+
+        if (!product) {
+          res.status(404);
+          throw new Error(`Product not found with id: ${item.product}`);
+        }
+        return {
+          qty: item.qty,
+          product: item.product,
+          name: product.name,
+          image: product.image, 
+          price: product.price,
+        };
+      })
+    );
+    // calculate prices
+    const itemsPrice = orderItemsWithRealData.reduce(
+      (acc, item) => acc + item.price * item.qty,
+      0
+    );
+    // shipping price fixed
+    const shippingPrice = 20000; 
+    // total price
+    const totalPrice = itemsPrice + shippingPrice;
+
     const order = new Order({
-      orderItems,
+      orderItems: orderItemsWithRealData,
       user: req.user._id,
       shippingAddress,
       paymentMethod,
