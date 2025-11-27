f9ceb2454c365f6020b9ad4e4d009c80ce916f1e
ivalhad <ivan14hadrian@gmail.com>
2025-11-27 11:55:08 +0700

    add getOrderById function and update order routes for fetching order details

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -58,7 +58,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
+// get detail order by id
+const getOrderById = asyncHandler(async(req, res) => {
+  const order = await Order.findById(req.params.id).populate(user, 'name email');
+
+  if(order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+
+});
+
 
 module.exports = {
     addOrderItems,
+    getOrderById,
 };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { addOrderItems} = require ('../controllers/orderController');
+const { 
+  addOrderItems, 
+  getOrderById
+} = require ('../controllers/orderController');
 const { protect, admin } = require ('../middleware/authMiddleware');
 
 router.route('/')
   .post(protect, addOrderItems) // user create order
+
+
+router.route('/:id').get(protect, getOrderById); // get order by id
 
   module.exports = router;
