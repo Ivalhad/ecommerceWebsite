3cd167658e242efed01c76ba96a26048b8da634e
ivalhad <ivan14hadrian@gmail.com>
2025-11-27 23:24:57 +0700

    add route fot getMyCart in routes

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
   addToCart,
+  getMyCart
 } = require('../controllers/cartController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/')
-  .post(protect, addToCart)   // add product to cart 
+  .post(protect, addToCart) // add product to cart
+  .get(protect, getMyCart ) // get product in cart
 
 module.exports = router;
