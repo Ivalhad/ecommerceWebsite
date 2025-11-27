d8e4add0659b3b6b266e1485ea661b83060900ab
ivalhad <ivan14hadrian@gmail.com>
2025-11-27 23:37:18 +0700

    add route delete product by id in cart in routes

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {
   addToCart,
   getMyCart,
+  removeItemFromCart,
   clearCart,
 } = require('../controllers/cartController');
 const { protect } = require('../middleware/authMiddleware');
@@ -11,5 +12,7 @@ router.route('/')
   .post(protect, addToCart) // add product to cart
   .get(protect, getMyCart ) // get product in cart
   .delete(protect, clearCart ) // delete all product in cart
+
+router.route('/:id').delete(protect, removeItemFromCart) // delete product by id in cart
 
 module.exports = router;
