421ddc354b1689f3e731376e0625f24286f5d55d
ivalhad <ivan14hadrian@gmail.com>
2025-11-27 13:09:13 +0700

    add updaterOrderToPaid function in order controller and add route order pain in routes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -71,8 +71,26 @@ const getOrderById = asyncHandler(async(req, res) => {
 
 });
 
+// update status to paid
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.status = 'Paid';
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
 
 module.exports = {
     addOrderItems,
     getOrderById,
+    updateOrderToPaid,
 };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { 
   addOrderItems, 
-  getOrderById
+  getOrderById,
+  updateOrderToPaid
 } = require ('../controllers/orderController');
 const { protect, admin } = require ('../middleware/authMiddleware');
 
@@ -11,5 +12,9 @@ router.route('/')
 
 
 router.route('/:id').get(protect, getOrderById); // get order by id
+
+
+// router update status
+router.route('/:id/pay').put(protect, admin, updateOrderToPaid);
 
 module.exports = router;
