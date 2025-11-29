53759c8a41a19465c24f9a58921beda5f422aaa1
ivalhad <ivan14hadrian@gmail.com>
2025-11-29 14:45:45 +0700

    fetch and display products on HomeScreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Product from '../components/Product';
 
 const HomeScreen = () => {
+  // state for save product
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      
+      setProducts(data.products); 
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Ini Halaman Utama (Dari File Terpisah)</h1>
-    </div>
+    <>
+      <h1 className="text-2xl font-bold mb-6 text-gray-700">Produk Terbaru</h1>
+      
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </>
   );
 };
 
