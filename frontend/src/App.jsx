76f97853cce6e2f0794b0f0a34b4ed39a56a95cd
ivalhad <ivan14hadrian@gmail.com>
2025-11-29 13:39:19 +0700

    refactor App component to remove unused code and simplify structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <header className="bg-gray-800 text-white p-4">
+        <div className="container mx-auto">SimplyShop Header</div>
+      </header>
 
-export default App
+      <main className="container mx-auto py-3 min-h-screen">
+        <Outlet />
+      </main>
+
+      <footer className="bg-gray-800 text-white text-center p-4 mt-10">
+        <p>SimplyShop &copy; {new Date().getFullYear()}</p>
+      </footer>
+
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
