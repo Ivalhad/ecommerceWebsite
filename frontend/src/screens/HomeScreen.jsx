de2c5e27ec82953479602f1c889b3e0a0635df3d
ivalhad <ivan14hadrian@gmail.com>
2025-12-09 20:13:32 +0700

    fetch products with pagination and keyword filtering; update header to display search results

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,47 @@
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Product from '../components/Product';
+import Paginate from '../components/Paginate'; 
 
 const HomeScreen = () => {
-  // state for save product
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(1);
+
+  // get keyword and page number
+  const { keyword, pageNumber } = useParams();
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
+      const { data } = await axios.get('/api/products', {
+        params: { 
+          keyword: keyword, 
+          pageNumber: pageNumber 
+        }
+      });
       
-      setProducts(data.products); 
+      setProducts(data.products);
+      setPage(data.page);
+      setPages(data.pages);
     };
 
     fetchProducts();
-  }, []);
+  }, [keyword, pageNumber]);
 
   return (
     <>
-      <h1 className="text-2xl font-bold mb-6 text-gray-700">Produk Terbaru</h1>
+      <h1 className="text-2xl font-bold mb-6 text-gray-700">
+        {keyword ? `Hasil Pencarian: "${keyword}"` : 'Produk Terbaru'}
+      </h1>
       
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <Product key={product._id} product={product} />
         ))}
       </div>
+
+      <Paginate pages={pages} page={page} keyword={keyword ? keyword : ''} />
     </>
   );
 };
