62b2bfed22555da4d1119d66d20f34edb95adb96
ivalhad <ivan14hadrian@gmail.com>
2025-12-10 15:34:25 +0700

    add admin route in components

---
+++ frontend/src/components/AdminRoute.jsx
@@ -0,0 +1,15 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const AdminRoute = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+
+  // check admin
+  return userInfo && userInfo.role === 'admin' ? (
+    <Outlet /> 
+  ) : (
+    <Navigate to="/login" replace />
+  );
+};
+
+export default AdminRoute;
